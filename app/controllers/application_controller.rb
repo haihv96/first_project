03406dbabef70404cb723c27ba52cb46df49f161
hoanghai096 <hai.hp.96@gmail.com>
2017-06-07 23:14:15 +0700

    chap 7 sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def render_404
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+  end
+
+  def render_500
+    render file: "#{Rails.root}/public/500.html", layout: false, status: 500
+  end
+
+  def render_422
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 422
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: :show
+
   def new
+    @user = User.new
+  end
+
+  def show
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t ".success"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".error"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+      .permit :name, :email, :password, :password_confirmation
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    render_404 unless @user
   end
 end
