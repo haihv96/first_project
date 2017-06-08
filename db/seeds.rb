199d50e098e859c5a63eb45cc0cbf0f0923ef1d3
hoanghai096 <hai.hp.96@gmail.com>
2017-06-08 13:45:46 +0700

    chap 8

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+User.create name: "Hoàng Hải",
+  email: "hai.hp.96@gmail.com",
+  password: "123456"
+
+100.times do |index|
+  User.create name: FFaker::NameVN.name,
+    email: "example-#{index+1}@railstutorial.org",
+    password: "123456"
+end
